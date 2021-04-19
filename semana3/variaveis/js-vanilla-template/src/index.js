ee310bab7ab12ad130e3743564002dd58a4a8356
Sarah Migliorini <smigliorinirocha@gmail.com>
2021-04-19 16:55:04 -0300

    exercícios de variáveis.

---
+++ semana3/variaveis/js-vanilla-template/src/index.js
@@ -0,0 +1,13 @@
+/* Programa 1 */
+
+// console.log(b): É impresso o valor 10 (atribuido à b)
+
+// console.log(a, b): É impresso o valor 10 e 5 (pois nesse segundo log, foi atribuido b = 5)
+
+/* Programa 2 */
+
+const nome = prompt("Me conte seu nome:")
+let idade = prompt("Agora, sua idade:")
+
+console.log(typeof nome)
+console.log(typeof idade)
